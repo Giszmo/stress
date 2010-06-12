7cac1a9e72e76fe223b6d1b75fbf6a7ba5e05516
Leo Wandersleb <Leo@LeoWandersleb.de>
2010-06-12 13:26:33 +0200

    init

---
+++ src/com/droidwave/stress/Stress.java
@@ -0,0 +1,13 @@
+package com.droidwave.stress;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Stress extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
