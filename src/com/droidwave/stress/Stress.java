94554a430e84511330573e52ae5151b7f2294006
Leo Wandersleb <Leo@LeoWandersleb.de>
2010-06-13 15:55:18 +0200

    click handler und Player-Ermittlung

--- src/com/droidwave/stress/Stress.java
+++ src/com/droidwave/stress/Stress.java
@@ -2,12 +2,52 @@ package com.droidwave.stress;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Stress extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// Watch for button clicks.
+		int[] buttonIds = { R.id.Button01P1, R.id.Button02P1, R.id.Button03P1,
+				R.id.Button04P1, R.id.Button01P2, R.id.Button02P2,
+				R.id.Button03P2, R.id.Button04P2 };
+		for (int buttonId : buttonIds) {
+			Button button = (Button) findViewById(buttonId);
+			button.setOnClickListener(clickListener);
+		}
+	}
+
+	private OnClickListener clickListener = new OnClickListener() {
+
+		public void onClick(View v) {
+			Button button = (Button) v;
+			String player = getPlayerByButton(button);
+			String message = player + " pressed button " + button.getText();
+			Toast.makeText(Stress.this, message, Toast.LENGTH_SHORT).show();
+		}
+
+		private String getPlayerByButton(Button button) {
+			String player = null;
+			switch (button.getId()) {
+			case R.id.Button01P1:
+			case R.id.Button02P1:
+			case R.id.Button03P1:
+			case R.id.Button04P1:
+				player = "Player 1";
+				break;
+			case R.id.Button01P2:
+			case R.id.Button02P2:
+			case R.id.Button03P2:
+			case R.id.Button04P2:
+				player = "Player 2";
+				break;
+			}
+			return player;
+		}
+	};
 }
