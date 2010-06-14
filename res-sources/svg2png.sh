0127f6e4eb1c6278ba7b6bd31f0315136cf8670d
Leo Wandersleb <Leo@LeoWandersleb.de>
2010-06-14 03:06:41 +0200

    icon und script

---
+++ res-sources/svg2png.sh
@@ -0,0 +1,3 @@
+convert -background none icon.svg -resize 36x36 ../res/drawable-ldpi/icon.png
+convert -background none icon.svg -resize 48x48 ../res/drawable-mdpi/icon.png
+convert -background none icon.svg -resize 72x72 ../res/drawable-hdpi/icon.png
