1f3e382a4087c303229d73e7bac9ba1f25e8ac54
Leo Wandersleb <Leo@LeoWandersleb.de>
2010-12-03 01:20:09 +0100

    new icons

--- res-sources/svg2png.sh
+++ res-sources/svg2png.sh
@@ -1,3 +1,5 @@
 convert -background none icon.svg -resize 36x36 ../res/drawable-ldpi/icon.png
 convert -background none icon.svg -resize 48x48 ../res/drawable-mdpi/icon.png
 convert -background none icon.svg -resize 72x72 ../res/drawable-hdpi/icon.png
+convert -background none icon.svg -resize 512x512 uhdpi_icon.png
+
